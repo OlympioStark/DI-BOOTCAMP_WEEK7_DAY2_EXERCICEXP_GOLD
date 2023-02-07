bbefa1e81a929dcaf9e67c34dc9db458343b7beb
OlympioStark <melessgilleschrist@gmail.com>
2023-02-07 20:48:27 +0000

    Exercice XP Gold Day 2

---
+++ src/Exercice1/Point.java
@@ -0,0 +1,50 @@
+package Exercice1;
+
+public class Point {
+	
+	private int x;
+	private int y;
+	
+	public Point() {
+		super();
+	}
+	
+	public Point (int x, int y) {
+		super();
+		this.x =x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	@Override
+	public String toString () {
+		return "(" + x + ", " + y + ")";
+	}
+
+	public int[] getXY() {
+		return new int[] {getX(), getY()};
+	}
+	
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distance(int x, int y) {
+		double dist = Math.sqrt(Math.pow((this.x - x), 2) + Math.pow((this.y - y), 2));
+		
+		return dist;
+	}
+}
