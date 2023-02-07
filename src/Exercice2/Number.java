ab40d0048f6835e5d6588d393dfd1038beffd451
OlympioStark <melessgilleschrist@gmail.com>
2023-02-07 21:41:24 +0000

    Exercice XP Gold Day2

---
+++ src/Exercice2/Number.java
@@ -0,0 +1,15 @@
+package Exercice2;
+
+public class Number {
+
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
---
+++ src/Exercice3/Main.java
@@ -0,0 +1,11 @@
+package Exercice3;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Rectangle rectangle = new Rectangle(5, 6);
+        System.out.println(rectangle);
+	}
+
+}
